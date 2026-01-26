62142068a8230325b4cd49bf7be089dda4117675
Andie2005-gith <andiemiarintsoa05@gmail.com>
2026-01-26 07:54:52 +0300

    Create FactureReglement.java

---
+++ src/main/java/entity/FactureReglement.java
@@ -0,0 +1,42 @@
+package entity;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "facture_reglement")
+public class FactureReglement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_facture_reglement")
+    private Integer idFactureReglement;
+
+    @Column(name = "id_facture", nullable = false)
+    private Integer idFacture;
+
+    @Column(name = "id_reglement", nullable = false)
+    private Integer idReglement;
+
+    @Column(name = "montant_affecte", nullable = false)
+    private BigDecimal montantAffecte;
+
+    @Column(name = "date_affectation")
+    private LocalDateTime dateAffectation;
+
+    public Integer getIdFactureReglement() { return idFactureReglement; }
+    public void setIdFactureReglement(Integer idFactureReglement) { this.idFactureReglement = idFactureReglement; }
+
+    public Integer getIdFacture() { return idFacture; }
+    public void setIdFacture(Integer idFacture) { this.idFacture = idFacture; }
+
+    public Integer getIdReglement() { return idReglement; }
+    public void setIdReglement(Integer idReglement) { this.idReglement = idReglement; }
+
+    public BigDecimal getMontantAffecte() { return montantAffecte; }
+    public void setMontantAffecte(BigDecimal montantAffecte) { this.montantAffecte = montantAffecte; }
+
+    public LocalDateTime getDateAffectation() { return dateAffectation; }
+    public void setDateAffectation(LocalDateTime dateAffectation) { this.dateAffectation = dateAffectation; }
+}
